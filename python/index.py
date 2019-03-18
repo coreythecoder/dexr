0e07d61559a78b7bd35e37a378ecfb90626eb697
coreythecoder <31457054+coreythecoder@users.noreply.github.com>
2019-03-18 16:49:03 -0400

    merge

--- python/index.py
+++ python/index.py
@@ -1,9 +1,17 @@
 #!C:\Python34\python.exe
 print()
 
+import cgi
 import pymysql
 import os as application
 from berserker_resolver import Resolver
+
+GET={}
+args=application.getenv("QUERY_STRING").split('&')
+
+for arg in args: 
+    t=arg.split('=')
+    if len(t)>1: k,v=arg.split('='); GET[k]=v
 
 host="dexr-production-2.ci0hkmciekd6.us-east-2.rds.amazonaws.com"
 port="3306"
@@ -15,19 +23,19 @@ db="ebdb"
 conn = pymysql.connect(host=host, port=3306, user=user, passwd=passwd, db=db)
 
 cur = conn.cursor()
+cur2 = conn.cursor()
+cur3 = conn.cursor()
 
-cur.execute("SELECT domain_name FROM user_1_3526197866 LIMIT 1000")
+cur.execute("SELECT domain_name FROM " + GET.get('table') + " WHERE crawled = '0' LIMIT 500")
 
 domains = []
 
 if cur is not None:
     for row in cur:
         domains.append(row[0])
+        cur3.execute("UPDATE " + GET.get('table') + " SET crawled = 1 WHERE domain_name = '" + row[0] + "'")
 else:
     response = b"cur is empty"
-
-conn.commit()
-cur.close()
 
 resolver = Resolver(www=True, www_combine=True, verbose=True, timeout=2, tries=1, threads=500)
 result = resolver.resolve(domains)
@@ -38,8 +46,14 @@ if result['success'] is not None:
     for d in result['success']:
         if d in domains:
             activeDomains.append(d)
+            cur2.execute("UPDATE " + GET.get('table') + " SET active = 1 WHERE domain_name = '" + d + "'")
 else:
     response = b"d is empty"
+
+conn.commit()
+cur2.close()
+cur.close()
+cur3.close()
 
 response = ' '.join(activeDomains)
 
@@ -48,3 +62,4 @@ if response is None:
 else:
     print(response)
     
+print('<meta http-equiv="refresh" content="1">')
